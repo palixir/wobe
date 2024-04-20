6855f95cf4d232ba85d274aaa5803e4ceae0ce28
coratgerl <mr.palixir@gmail.com>
2024-04-20 18:15:45 +0200

    refactor: remove integration test

--- packages/wobe/src/Wobe.test.ts
+++ packages/wobe/src/Wobe.test.ts
@@ -14,6 +14,7 @@ import { HttpException } from './HttpException'
 import type { Context } from './Context'
 import * as nodeAdapter from './adapters/node'
 import * as bunAdapter from './adapters/bun'
+import { bearerAuth, csrf, logger } from './hooks'
 
 describe('Wobe', () => {
 	let wobe: Wobe
@@ -39,6 +40,8 @@ describe('Wobe', () => {
 	})
 	const mockOnError = mock(() => {})
 	const mockOnNotFound = mock(() => {})
+	const mockHookWithWildcardRoute = mock(() => {})
+	const spyConsoleLog = spyOn(console, 'log').mockResolvedValue({} as never)
 
 	let port: number
 
@@ -49,47 +52,82 @@ describe('Wobe', () => {
 			onError: mockOnError,
 			onNotFound: mockOnNotFound,
 		})
+			.get('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+			.post('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+			.put('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+			.delete('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+			.all('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+			.get('/route/:id/name', (ctx) => {
+				if (ctx.query.test) return ctx.res.sendText(ctx.query.test)
 
-		wobe.get('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
-		wobe.post('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
-		wobe.put('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
-		wobe.delete('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
-		wobe.all('/testRouteWithHook', () => {}, mockHookOnSpecificRoute)
+				return ctx.res.sendText(ctx.params.id)
+			})
+			.get('/testGet', (ctx) => {
+				mockTestGet()
+				return ctx.res.send('Test')
+			})
+			.get('/ipAdress', (ctx) => {
+				ctx.res.send(ctx.getIpAdress())
+			})
+			.post('/testPost', (ctx) => {
+				mockTestPost()
+				return ctx.res.send('Tata')
+			})
+			.put('/testPut', (ctx) => {
+				mockTestPut()
+				return ctx.res.send('Put')
+			})
+			.delete('/testDelete', (ctx) => {
+				mockTestDelete()
+				return ctx.res.send('Delete')
+			})
+			.all('/allMethod', (ctx) => {
+				mockAllMethod()
+				return ctx.res.send('All')
+			})
+			.get('/test/v1', (ctx) => {
+				return ctx.res.send('Test')
+			})
+			.get('/testReturnResponse', () => {
+				return new Response('Content', { status: 200 })
+			})
+			.get('/test', (ctx) => {
+				return ctx.res.send('Test')
+			})
+			.get('/testBearer', (ctx) => {
+				return ctx.res.send('Test')
+			})
+			.get('/testHookLifecyce', (ctx) => {
+				ctx.res.headers.set('X-Test-2', 'Test2')
 
-		wobe.get('/testGet', (ctx) => {
-			mockTestGet()
-			return ctx.res.send('Test')
-		})
+				return ctx.res.send('Test')
+			})
+			.get('/testStatusText', (ctx) => {
+				ctx.res.statusText = 'Test'
 
-		wobe.post('/testPost', (ctx) => {
-			mockTestPost()
-			return ctx.res.send('Tata')
-		})
-
-		wobe.put('/testPut', (ctx) => {
-			mockTestPut()
-			return ctx.res.send('Put')
-		})
-
-		wobe.delete('/testDelete', (ctx) => {
-			mockTestDelete()
-			return ctx.res.send('Delete')
-		})
-
-		wobe.all('/allMethod', (ctx) => {
-			mockAllMethod()
-			return ctx.res.send('All')
-		})
-
-		wobe.usePlugin(mockUsePlugin())
-
-		wobe.beforeHandler(mockHook)
-		wobe.beforeHandler(mockSecondHook)
-		wobe.beforeHandler(mockHookOnlyBeforeHandler)
-		wobe.beforeHandler('/testGet', mockOnlyOnTestGet)
-		wobe.beforeAndAfterHandler(mockHookBeforeAndAfterHandler)
-
-		wobe.listen(port)
+				return ctx.res.send('Test')
+			})
+			.usePlugin(mockUsePlugin())
+			.beforeHandler(
+				'/test/',
+				csrf({ origin: `http://127.0.0.1:${port}` }),
+			)
+			.beforeAndAfterHandler(logger())
+			.beforeHandler('/testBearer', bearerAuth({ token: '123' }))
+			.beforeHandler('/test/*', mockHookWithWildcardRoute)
+			.beforeHandler(mockHook)
+			.beforeHandler(mockSecondHook)
+			.beforeHandler(mockHookOnlyBeforeHandler)
+			.beforeHandler('/testGet', mockOnlyOnTestGet)
+			.beforeAndAfterHandler(mockHookBeforeAndAfterHandler)
+			.beforeHandler('/testHookLifecyce', (ctx) => {
+				ctx.res.headers.set('X-Test', 'Test')
+			})
+			.afterHandler('/testHookLifecyce', (ctx) => {
+				ctx.res.headers.set('X-Test-3', 'Test3')
+				return ctx.res.send('Test after handler')
+			})
+			.listen(port)
 	})
 
 	afterAll(() => {
@@ -111,6 +149,8 @@ describe('Wobe', () => {
 		mockTestDelete.mockClear()
 		mockOnError.mockClear()
 		mockOnNotFound.mockClear()
+		mockHookWithWildcardRoute.mockClear()
+		spyConsoleLog.mockClear()
 	})
 
 	it.skipIf(process.env.NODE_TEST !== 'true')(
@@ -138,6 +178,134 @@ describe('Wobe', () => {
 			wobe.stop()
 		},
 	)
+
+	// Waiting https://github.com/oven-sh/bun/pull/10266
+	it.skip('should return the good statusText of the response', async () => {
+		spyConsoleLog.mockRestore()
+
+		const res = await fetch(`http://127.0.0.1:${port}/testStatusText`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(res.statusText).toBe('Test')
+	})
+
+	it('should have a beforeHandler and afterHandler hook and a route that update response', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/testHookLifecyce`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(res.status).toBe(200)
+		expect(res.headers.get('X-Test')).toBe('Test')
+		expect(res.headers.get('X-Test-2')).toBe('Test2')
+		expect(res.headers.get('X-Test-3')).toBe('Test3')
+		expect(await res.text()).toBe('Test after handler')
+	})
+
+	it('should log with logger hook', async () => {
+		await fetch(`http://127.0.0.1:${port}/test`, {
+			method: 'GET',
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(spyConsoleLog).toHaveBeenCalledTimes(2)
+		expect(spyConsoleLog).toHaveBeenNthCalledWith(
+			1,
+			`[Before handler] [GET] http://127.0.0.1:${port}/test`,
+		)
+
+		// We don't use toHaveBeenNthCalledWith to avoid flaky on execution time
+		expect(spyConsoleLog.mock.calls[1][0]).toContain(
+			`[After handler] [GET] http://127.0.0.1:${port}/test (status:200)`,
+		)
+		expect(spyConsoleLog.mock.calls[1][0]).toContain('ms]')
+	})
+
+	it('should block the request if the bearer auth is not valid', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/testBearer`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+				Authorization: 'Bearer 1234',
+			},
+		})
+
+		expect(res.status).toBe(401)
+		expect(res.statusText).toBe('Unauthorized')
+		expect(res.headers.get('WWW-Authenticate')).toEqual(
+			'Bearer realm="", error="invalid_token"',
+		)
+	})
+
+	it('should not block requests with valid origin', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/test`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(res.status).toBe(200)
+	})
+
+	it('should block requests with invalid origin', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/test`, {
+			headers: {
+				origin: 'invalid-origin',
+			},
+		})
+
+		expect(res.status).toBe(403)
+		expect(res.statusText).toBe('Forbidden')
+		expect(await res.text()).toBe('CSRF: Invalid origin')
+	})
+
+	it('should return a response directly from a route', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/testReturnResponse`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(await res.text()).toEqual('Content')
+		expect(res.status).toBe(200)
+	})
+
+	it('should execute hooks with a route name like /test/* for any route begin by /test/', async () => {
+		await fetch(`http://127.0.0.1:${port}/test/v1`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(mockHookWithWildcardRoute).toHaveBeenCalledTimes(1)
+	})
+
+	it('should get the route params on a route with parameters', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/route/1/name`)
+
+		expect(await res.text()).toEqual('1')
+
+		const res2 = await fetch(
+			`http://127.0.0.1:${port}/route/1/name?test=bun`,
+		)
+
+		expect(await res2.text()).toEqual('bun')
+	})
+
+	it('should return the ip adress of the client', async () => {
+		const res = await fetch(`http://127.0.0.1:${port}/ipAdress`, {
+			headers: {
+				origin: `http://127.0.0.1:${port}`,
+			},
+		})
+
+		expect(await res.text()).toBe('::ffff:127.0.0.1')
+	})
 
 	it('should call a hook on a specific route', async () => {
 		await fetch(`http://127.0.0.1:${port}/testRouteWithHook`, {
