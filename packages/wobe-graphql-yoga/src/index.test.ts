cb6fb86543f1a645740c7d70e236be9ad73866a4
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-06-04 20:12:37 +0200

    feat: add test and types

--- packages/wobe-graphql-yoga/src/index.test.ts
+++ packages/wobe-graphql-yoga/src/index.test.ts
@@ -5,6 +5,59 @@ import getPort from 'get-port'
 import { WobeGraphqlYogaPlugin } from '.'
 
 describe('Wobe GraphQL Yoga plugin', () => {
+	it("should use the graphql middleware if it's provided", async () => {
+		const port = await getPort()
+		const wobe = new Wobe()
+
+		wobe.usePlugin(
+			WobeGraphqlYogaPlugin({
+				graphqlMiddleware: async (resolve, res) => {
+					const response = await resolve
+					res.setCookie('test', 'test')
+
+					return response
+				},
+				typeDefs: `
+            type Query {
+              hello: String
+            }
+          `,
+				resolvers: {
+					Query: {
+						hello: (_, __, context) => {
+							expect(context.request.headers).toBeDefined()
+							return 'Hello from Yoga!'
+						},
+					},
+				},
+			}),
+		)
+
+		wobe.listen(port)
+
+		const res = await fetch(`http://127.0.0.1:${port}/graphql`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				query: `
+				  query {
+        hello
+      }
+        `,
+			}),
+		})
+
+		expect(res.status).toBe(200)
+		expect(res.headers.get('set-cookie')).toBe('test=test')
+		expect(await res.json()).toEqual({
+			data: { hello: 'Hello from Yoga!' },
+		})
+
+		wobe.stop()
+	})
+
 	it('should work with typedef and resolvers', async () => {
 		const port = await getPort()
 		const wobe = new Wobe()
--- packages/wobe-graphql-yoga/src/index.ts
+++ packages/wobe-graphql-yoga/src/index.ts
@@ -5,20 +5,29 @@ import {
 	type YogaServerOptions,
 	type YogaInitialContext,
 } from 'graphql-yoga'
-import type { MaybePromise, Wobe, WobePlugin } from 'wobe'
+import type { MaybePromise, Wobe, WobePlugin, WobeResponse } from 'wobe'
 
 export type GraphqlYogaContext =
 	| MaybePromise<Record<string, unknown>>
 	| ((initialContext: YogaInitialContext) => MaybePromise<unknown>)
 
+export interface GraphqlYogaPluginOptions {
+	graphqlMiddleware?: (
+		resolve: Promise<WobeResponse>,
+		res: WobeResponse,
+	) => Promise<WobeResponse>
+}
+
 export const WobeGraphqlYogaPlugin = ({
+	graphqlMiddleware,
 	...options
 }: {
 	schema?: GraphQLSchemaWithContext<Record<string, any>>
 	typeDefs?: string
 	context?: GraphqlYogaContext
 	resolvers?: Record<string, any>
-} & Omit<YogaServerOptions<any, any>, 'context'>): WobePlugin => {
+} & Omit<YogaServerOptions<any, any>, 'context'> &
+	GraphqlYogaPluginOptions): WobePlugin => {
 	const yoga = createYoga({
 		...options,
 		schema:
@@ -30,8 +39,13 @@ export const WobeGraphqlYogaPlugin = ({
 	})
 
 	return (wobe: Wobe) => {
-		wobe.get(options?.graphqlEndpoint || '/graphql', async ({ request }) =>
-			yoga.fetch(request),
+		wobe.get(
+			options?.graphqlEndpoint || '/graphql',
+			async ({ request, res }) => {
+				if (!graphqlMiddleware) return fetch(request)
+
+				return graphqlMiddleware(new Promise(() => fetch(request)), res)
+			},
 		)
 		wobe.post(options?.graphqlEndpoint || '/graphql', async ({ request }) =>
 			yoga.fetch(request),
