390781c95ccef4e481a6c50694b05c8892124ec7
coratgerl <73360179+coratgerl@users.noreply.github.com>
2024-06-23 08:02:17 +0200

    wip

--- packages/wobe-graphql-upload/src/index.ts
+++ packages/wobe-graphql-upload/src/index.ts
@@ -1,14 +1,14 @@
 import type { Context, WobeHandler } from 'wobe'
-import { processRequest, type UploadOptions } from 'graphql-upload-minimal'
+import { processRequest, type UploadOptions } from 'graphql-upload-ts'
 
 export const wobeGraphqlUpload = (options?: UploadOptions): WobeHandler => {
 	return async ({ request, res }: Context) => {
-		if (
-			request.headers.get('Content-Disposition') !== 'multipart/form-data'
-		)
-			return
-
+		// if (
+		// 	request.headers.get('Content-Disposition') !== 'multipart/form-data'
+		// )
+		// 	return
+		// console.log(await request.formData())
 		// @ts-expect-error This is conventional
-		request.body = await processRequest(request, res.response, options)
+		request.body = await processRequest(request, new Response(), options)
 	}
 }
